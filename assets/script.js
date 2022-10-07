547efe75ead3e3e55287f881ce5b55634c95462c
Eric Simmons <erictomlinsonsimmons@gmail.com>
2022-10-06 19:15:50 -0500

    buttons and base html

---
+++ assets/script.js
@@ -0,0 +1,68 @@
+//psuedo code
+var charactersArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w','x', 'y', 'z']
+//Get all elements from HTML
+var btn = document.getElementById("playButton")
+var btn = document.getElementById("stopButton")
+
+//audio files
+var audioB = new Audio('./assets/sounds/bell.wav');
+var audioK = new Audio('./assets/sounds/kick.wav');
+
+const bpm = setInterval(playBeat, 1000);
+
+//Search bar
+//Lyrics container 
+//Background div 
+//Play button/Pause button
+//Sliders
+//Save Settings button
+
+//add event listeners
+playButton.addEventListener('click', playBeat)
+stopButton.addEventListener('click', stopBeat)
+
+const synth = new Tone.Synth().toDestination();
+
+//play a middle 'C' for the duration of an 8th note
+synth.triggerAttackRelease("C4", "8n");
+//play button
+//pause button
+//sliders
+
+
+
+
+
+
+
+//Access Lyrics API
+
+//Fetch call to API
+//Set up search bar value to concat into link
+//Display lyrics in lyric container
+
+//Interval
+
+//Play Beat Function
+
+
+// function playBeat() {
+
+// for ( i = 0; i < charactersArray.length; i++){
+    
+
+//     if(charactersArray[i] === 'b'){
+//         audioB.play();
+//         console.log('hey')
+//     }
+//     else if(charactersArray[i] === 'k'){
+//         audioK.play();
+//         console.log('hi')
+//     }
+// }
+// }
+
+function stopBeat() {
+    clearInterval(bpm);
+  }
+
