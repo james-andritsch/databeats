609da39f44688dcf88d7e137ec52b8a017b78f9f
Eric Simmons <erictomlinsonsimmons@gmail.com>
2022-10-06 20:43:36 -0500

    saving

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,16 @@
+
+var APIKey = "bb8b453155a29d851c8d0280a24261a1"
+
+// var queryURL = "https://octoproxymus.herokuapp.com?secret=walrus&url=https://api.musixmatch.com/ws/1.1/?apikey=" + APIKey;
+var queryURL = "https://octoproxymus.herokuapp.com?secret=walrus&url=https://www.stands4.com/services/v2/lyrics.php?uid=1001&tokenid=tk324324&term=forever%20young&artist=Alphaville&format=json"
+var formatURL = "?format=json&callback=callback"
+
+
 //psuedo code
-var charactersArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w','x', 'y', 'z']
+var charactersArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 //Get all elements from HTML
-var btn = document.getElementById("playButton")
-var btn = document.getElementById("stopButton")
+var playButton = document.getElementById("playButton")
+var stopButton = document.getElementById("stopButton")
 
 //audio files
 var audioB = new Audio('./assets/sounds/bell.wav');
@@ -17,14 +25,11 @@ const bpm = setInterval(playBeat, 1000);
 //Sliders
 //Save Settings button
 
-//add event listeners
-playButton.addEventListener('click', playBeat)
-stopButton.addEventListener('click', stopBeat)
 
-const synth = new Tone.Synth().toDestination();
 
-//play a middle 'C' for the duration of an 8th note
-synth.triggerAttackRelease("C4", "8n");
+
+
+
 //play button
 //pause button
 //sliders
@@ -38,6 +43,20 @@ synth.triggerAttackRelease("C4", "8n");
 //Access Lyrics API
 
 //Fetch call to API
+// fetch(queryURL)
+//     .then(function (response) {
+//         if (response.status === 200) {
+//             return response.json()
+//         }
+//         else if (response.status === 404) {
+//             alert('lyrics not found!')
+//         }
+//     })
+//     //retreive data
+//     .then(function (lyrics) {
+//         console.log(lyrics)
+//     })
+
 //Set up search bar value to concat into link
 //Display lyrics in lyric container
 
@@ -46,23 +65,31 @@ synth.triggerAttackRelease("C4", "8n");
 //Play Beat Function
 
 
-// function playBeat() {
+function playBeat() {
+    clearInterval(bpm);
+    // const synth = new Tone.Synth().toDestination();
+    // synth.triggerAttackRelease("C4", "8n");
 
-// for ( i = 0; i < charactersArray.length; i++){
-    
+    for (var i = 0; i < charactersArray.length; i++) {
 
-//     if(charactersArray[i] === 'b'){
-//         audioB.play();
-//         console.log('hey')
-//     }
-//     else if(charactersArray[i] === 'k'){
-//         audioK.play();
-//         console.log('hi')
-//     }
-// }
-// }
+
+        if (charactersArray[i] === 'b') {
+            audioB.play();
+            console.log('hey')
+        }
+        else if (charactersArray[i] === 'k') {
+            audioK.play();
+            console.log('hi')
+        }
+    }
+}
 
 function stopBeat() {
     clearInterval(bpm);
-  }
+}
 
+
+
+//add event listeners
+playButton.addEventListener('click', playBeat)
+stopButton.addEventListener('click', stopBeat)
