dc837d574a63d0fb12a40ef55988fa8318b002fb
Eric Simmons <erictomlinsonsimmons@gmail.com>
2022-10-12 21:16:54 -0500

    working string loop

--- assets/tonescript.js
+++ assets/tonescript.js
@@ -4,11 +4,45 @@ var stopButton = document.getElementById("stopButton")
 
 const player = new Tone.Player("assets/sounds/bell.wav");
 player.autostart = true;
+notes = []
+// var A = new Tone.Player('./assets/sounds/bell.wav').toDestination();
+// var B = new Tone.Player('./assets/sounds/kick.wav').toDestination();
 
-var A = new Tone.Player('./assets/sounds/bell.wav').toDestination();
-var B = new Tone.Player('./assets/sounds/kick.wav').toDestination();
+// function searchAPI() {
 
-let multiPlayer;
+
+//     var APIKey = ""
+//     var apiURL = "" + city + "&appid=" + APIKey;
+//     //parse readable stream into JSON
+//     fetch(forecastWeatherURL)
+//         .then(function (response) {
+//             if (response.status === 200) {
+//                 return response.json()
+//             }
+//             else if (response.status !== 200) {
+//                 alert('City not found!')
+//             }
+//         })
+//         //retreive data
+//         .then(function (forecast) {
+//             //put data into weather obj
+//             //clear weather array
+
+//             weatherArr = []
+//             for (var i = 0; i < 40; i += 8) {
+//                 var weatherObj = {
+//                     date: DateTime.fromSeconds(forecast.list[i].dt).toFormat('LLL dd'),
+//                     city: forecast.city.name,
+//                     temp: Math.round(((forecast.list[i].main.temp - 273.15) * 9 / 5 + 32)) + "°F",
+//                     wind: "Wind Speed " + forecast.list[i].wind.speed,
+//                     humidity: forecast.list[i].main.humidity + "%" + " Humidity",
+//                     condition: forecast.list[i].weather[0].description,
+//                     icon: "https://openweathermap.org/img/wn/" + forecast.list[i].weather[0].icon + "@2x.png",
+//                 }
+
+//                 weatherArr.push(weatherObj)
+
+//             }
 
 
 
@@ -38,40 +72,60 @@ const samples = new Tone.ToneAudioBuffers({
 
 })
 
-
-
-
-
-
-
-
-
 function play() {
+    Tone.Transport.stop()
+    Tone.Transport.start()
+    playButton.disabled = 'true'
+
+    
+    var searchString = searchEl.value.split("")
+    notes.push(searchString)
     
     const player = new Tone.Player().toDestination();
     player.buffer = samples.get("kick");
-    player.start();
-    
-    
-    
-    
-    
-    //var notes = ["C3", "G3", "D4", "E4", "B4", "C5", "G5"]
-    var notes = []
-    var searchString = searchEl.value.split("")
-    notes.push(searchString)
 
-    Tone.Transport.start()
-
-    const synth = new Tone.Synth().toDestination();
+    const player2 = new Tone.Player().toDestination();
+    player2.buffer = samples.get("shaker");
+    
+    
+    // new Tone.Loop(function (time) {
+       
+    //     // triggered every eighth note.
+    //     player.start
+    //     console.log(notes);
+    // }, "8n").start(0);
+  
     const seq = new Tone.Sequence(function (time, note) {
-        synth.triggerAttackRelease(note, 0.1, time);
+
+        if (note ==='a'){
+            player.start();
+            player.stop("+0.5"); 
+        }
+
+        if (note === 'b'){
+            player2.start();
+            player2.stop("+0.5")
+        }
+
+
         console.log(note)
     }, notes).start(0);
+    console.log(notes)
+    
+    
+  
+
+        console.log(notes)
+
+    console.log(notes)
+   
+  
 }
 
 function stop() {
+
     Tone.Transport.stop()
+
 }
 
 bpmSlider.addEventListener('input', function (event) {
